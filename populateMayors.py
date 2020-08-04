b3c88e73de697ad8387f7f0eaf64fd568a54fe43
kroscoe45 <56187219+kroscoe45@users.noreply.github.com>
2020-08-04 12:09:07 -0700

    bugfix

--- populateMayors.py
+++ populateMayors.py
@@ -6,10 +6,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from datetime import datetime
 
+i = 0
+
 data = pd.read_excel("master_sheet.xlsx")
 chromedriver = 'chromedriver.exe'
 driver = webdriver.Chrome(executable_path = f'{chromedriver}')
 driver.get("https://www.google.com/")
+lastMayor = ''
 
 def googleForMayor(city, state, category):
     searchbox = driver.find_element_by_name('q')
@@ -17,15 +20,25 @@ def googleForMayor(city, state, category):
     searchbox.send_keys('Who is the mayor of ' + city + ' ' + category +  ',' + ' ' + state)
     searchbox.send_keys(Keys.RETURN)
     try:
-        quickAnswer = driver.find_element_by_class_name('FLP8od')
-        return quickAnswer.text
+        quickAnswer = driver.find_element_by_class_name('FLP8od') #find quick answer if it is in standard text format
+        return quickAnswer.text + ' ??? FULL NAME'
     except:
         try: 
-            quickAnswer = driver.find_element_by_css_selector('div.Z0LcW.XcVN5d.AZCkJd')
-            return quickAnswer.text
+            quickAnswer = driver.find_element_by_css_selector('div.Z0LcW.XcVN5d.AZCkJd') #find quick answer if it is in standard text format
+            return quickAnswer.text + ' ??? FULL NAME'
         except:
-            try 
-            return np.nan
+            try:
+                quickAnswer = driver.find_element_by_class_name('webanswers-webanswers_table__webanswers-table') #find quick answer if it is in a table format
+                return quickAnswer.text + ' ??? TABLE FORMAT'
+            except:
+                try:
+                    topResultDescription = driver.find_elements_by_class_name('g') #return the top web result page description. Excludes wikipedia results
+                    if 'wikipedia' in topResultDescription[0].text:
+                        return topResultDescription[1].text
+                    else:
+                        return topResultDescription[0].text + ' ??? WEBPAGE DESCRIPTION'
+                except:
+                    return np.nan
 
 def sanitizeMayor(mayor):
     try:
@@ -40,10 +53,17 @@ for index, row in data.iterrows():
     state = data['State'].iloc[index]
     category = data['Type'].iloc[index]
     mayor = googleForMayor(city, state, category)
-    data['Mayor'].iloc[index] = sanitizeMayor(mayor)
-    if data['Mayor'].iloc[index] == lastMayor:
-        data['Mayor'].iloc[index] = np.nan
+    if 'FULL NAME' in mayor:
+        mayor = sanitizeMayor(mayor)
+    if mayor == lastMayor:
+        mayor = np.nan
+    data['Mayor'].iloc[index] = mayor
     time.sleep(1)
     lastMayor = data['Mayor'].iloc[index]
+    if i >= 1000:
+        data.to_excel('mayorOut.xlsx', index = False)
+        i = 0
+    i =  i + 1
+    print(i)
 
 data.to_excel('mayorOut.xlsx', index = False)
